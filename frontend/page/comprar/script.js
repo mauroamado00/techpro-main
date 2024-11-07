5349868e777a2904902239291581eed6a80e6f0f
Your Name <you@example.com>
2024-11-07 14:10:33 -0300

    f

--- frontend/page/comprar/script.js
+++ frontend/page/comprar/script.js
@@ -1,20 +1,10 @@
 import CarritoDAo from "../../dao/CarritoDao.js";
-<<<<<<< HEAD
-=======
-
->>>>>>> cd388766f64a9336c6f526ef1e6d3d569433fdfa
 
 window.onload =()=>{
     agregarevento();
 }
 
-<<<<<<< HEAD
 function agregarevento(){
-=======
-
-
-function addEvent(){
->>>>>>> cd388766f64a9336c6f526ef1e6d3d569433fdfa
     let metodoEnvio = document.querySelector("#metodoEnvio");
     let metodoPago = document.querySelector("#metodoPago");
     let confirmarCompraElement = document.querySelector("#realize-order");
@@ -32,7 +22,6 @@ function addEvent(){
 
     confirmarCompraElement.onsubmit = (e) => {
         e.preventDefault();
-<<<<<<< HEAD
         let nombrecompleto = confirmarCompraElement.nombrecompleto.value;
         let ciudad = confirmarCompraElement.ciudad.value;
         let numerodetelefono = confirmarCompraElement.numerodetelefono.value;
@@ -41,27 +30,13 @@ function addEvent(){
         let direccion = confirmarCompraElement.direccion.value;
         let metodoPago = confirmarCompraElement.direccion.value;
         confirmarCompra(nombrecompleto,ciudad,numerodetelefono,email,metodoEnvio,direccion,metodoPago);
-=======
-        let nombreCompleto = confirmarCompraElement.nombreCompleto.value;
-        let metodoPago = confirmarCompraElement.metodoPago.value;
-        let direccion = confirmarCompraElement.direccion.value;
-
-        confirmarCompra(nombreCompleto,metodoPago,direccion);
-
->>>>>>> cd388766f64a9336c6f526ef1e6d3d569433fdfa
         
     }
 }
 
-<<<<<<< HEAD
 async function confirmarCompra(nombrecompleto,ciudad,numerodetelefono,email,metodoEnvio,direccion,metodoPago){
     let carritoDAO = new CarritoDAo();
     let respuesta = await carritoDAO.confirmarCompra(nombrecompleto,ciudad,numerodetelefono,email,metodoEnvio,direccion,metodoPago)
-=======
-async function confirmarCompra(nombreCompleto,metodoPago,direccion){
-    let carritoDAO = new CarritoDAo();
-    let respuesta = await carritoDAO.confirmarCompra()
->>>>>>> cd388766f64a9336c6f526ef1e6d3d569433fdfa
     console.log(respuesta.menssage);
 
 }
